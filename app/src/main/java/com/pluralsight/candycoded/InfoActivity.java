af456d17269cf17f03d71fc43dee5397645da14e
kalu <kaluarichama@gmail.com>
2020-09-25 01:49:35 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,11 +36,15 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
+
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+        Intent dial = new Intent(Intent.ACTION_DIAL);
+        dial.setData(Uri.parse("tel:0123456789"));
+        startActivity(dial);
+    }
 }
